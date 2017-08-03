81c41dd471342b25a578d12b1faf4d9d2005fdac
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-08-03 13:20:56 -0500

    Add errors and time results

--- presenter/presenter.go
+++ presenter/presenter.go
@@ -1,26 +1,47 @@
 package presenter
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Result struct {
-	Task  string
-	Time  float64
-	Error error
+	Task   string
+	Time   float64
+	Error  error
+	Output string
 }
 
-func PrintResult(results []Result) {
+func PrintResult(results []Result, realTime float64) {
 	flag := false
 	message := ""
+	var total float64
 	for _, r := range results {
+		f := ""
+
 		if r.Error != nil {
 			flag = true
-		}
-		message = fmt.Sprintf("%s\n%s took %f", message, r.Task, r.Time)
-	}
+			logs := strings.Split(r.Output, "\n")
+			for i := range logs {
+				if strings.Contains(logs[i], "Failed tests:") {
+					for index := i + 1; index < len(logs); index++ {
+						if strings.Contains(logs[index], "Tests run:") {
+							break
+						}
+						f = fmt.Sprintf("%s\n%s", f, logs[index])
+					}
+					break
+				}
 
+			}
+		}
+		message = fmt.Sprintf("%s\n%s took %f%s", message, r.Task, r.Time, f)
+		total += r.Time
+	}
+	avg := total / float64(len(results))
 	if flag {
-		fmt.Printf("\n\n%s%s\n", "FAIL", message)
+		fmt.Printf("\n\n%s%s\nTOTAL: %f\nAVERAGE: %f\nREAL TIME: %f\n", "FAIL", message, total, avg, realTime)
 	} else {
-		fmt.Printf("\n\n%s%s\n", "SUCCESS", message)
+		fmt.Printf("\n\n%s%s\nTOTAL: %f\nAVERAGE: %f\nREAL TIME: %f\n", "SUCCESS", message, total, avg, realTime)
 	}
 }
