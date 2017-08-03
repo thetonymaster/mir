b88c4597c6361d6363b0ac8aa7c3ca5d53c5eb86
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-08-03 13:19:30 -0500

    Read logs from containers

--- provider/test/junit.go
+++ provider/test/junit.go
@@ -1,8 +1,11 @@
 package test
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"path"
 	"path/filepath"
@@ -10,6 +13,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/jeffail/tunny"
 	"github.com/thetonymaster/framework/presenter"
 	"github.com/thetonymaster/framework/provider/container"
@@ -28,21 +33,27 @@ type generator interface {
 
 // JUnit runs the JUnit tests
 type JUnit struct {
-	Generator generator
-	Target    string
-	pool      *tunny.WorkPool
-	Done      chan bool
-	Results   chan presenter.Result
+	Generator    generator
+	Target       string
+	pool         *tunny.WorkPool
+	Done         chan bool
+	Results      chan presenter.Result
+	dockerClient *client.Client
 }
 
 const JUnitProject = "junit"
 
 // NewJUnit creates a new instance of a JUnit task manager
 func NewJUnit(generator generator, target string, pool *tunny.WorkPool) *JUnit {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
 	return &JUnit{
-		Generator: generator,
-		Target:    target,
-		pool:      pool,
+		Generator:    generator,
+		Target:       target,
+		pool:         pool,
+		dockerClient: cli,
 	}
 }
 
@@ -66,10 +77,8 @@ func (junit *JUnit) RunTask(tasks []string) error {
 	containers.Run()
 
 	for _, task := range tasks {
-		payload := getPayload(containers, junit.Target, task, junit.Results)
+		payload := junit.getPayload(containers, junit.Target, task)
 		junit.pool.SendWorkAsync(payload, nil)
-		time.Sleep(2 * time.Second)
-
 	}
 	for junit.pool.NumPendingAsyncJobs() > 0 {
 		time.Sleep(1 * time.Second)
@@ -80,11 +89,14 @@ func (junit *JUnit) RunTask(tasks []string) error {
 	return nil
 }
 
-func getPayload(containers *container.Container, target, task string, results chan presenter.Result) func() {
+func random(min, max int64) int64 {
+	rand.Seed(time.Now().Unix())
+	return rand.Int63n(max-min) + min
+}
+
+func (junit *JUnit) getPayload(containers *container.Container, target, task string) func() {
 	return func() {
-		time.Sleep(3 * time.Second)
 		start := time.Now()
-		taskLocal := task
 		err := containers.Execute(target, "./mvnw", "test", fmt.Sprintf("-Dtest=%s", task))
 		elapsed := time.Since(start)
 		result := presenter.Result{
@@ -92,8 +104,35 @@ func getPayload(containers *container.Container, target, task string, results ch
 			Time:  elapsed.Seconds(),
 			Error: err,
 		}
-		results <- result
-		log.Printf("%s took %s\n", taskLocal, elapsed)
+		ctrs, _ := junit.dockerClient.ContainerList(context.Background(),
+			types.ContainerListOptions{
+				All: true,
+			})
+
+		id := ""
+		for _, ctr := range ctrs {
+			if strings.Contains(ctr.Command, task) {
+				id = ctr.ID
+				break
+			}
+		}
+
+		logs, _ := junit.dockerClient.ContainerLogs(context.Background(), id,
+			types.ContainerLogsOptions{
+				ShowStdout: true,
+				ShowStderr: true,
+				Tail:       "50",
+			})
+		defer logs.Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(logs)
+		result.Output = buf.String()
+
+		junit.dockerClient.ContainerRemove(context.Background(), id,
+			types.ContainerRemoveOptions{})
+
+		junit.Results <- result
+		log.Printf("%s took %s\n", task, elapsed)
 		if err != nil {
 			fmt.Println(err)
 		}
